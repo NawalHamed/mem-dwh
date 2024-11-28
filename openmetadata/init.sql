0cf7cfcf85d59fc3f31e0c4191db7ba815f7d75f
Nawal Hamed Al Mawali <65490811+NawalHamed@users.noreply.github.com>
2024-11-28 16:32:43 +0400

    Update init.sql

--- openmetadata/init.sql
+++ openmetadata/init.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE IF NOT EXISTS openmetadata_db;
-GRANT ALL PRIVILEGES ON openmetadata_db.* TO 'root'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON openmetadata_db.* TO 'openmetadata_user'@'%' IDENTIFIED BY 'openmetadata_password';
 FLUSH PRIVILEGES;
